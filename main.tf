3d92251f6eb71124e2696fdfbbcc15f74418ecb5
Peyton Casper <peytoncas@gmail.com>
2020-02-19 22:28:58 -0500

    Updated for count indexing

--- main.tf
+++ main.tf
@@ -8,36 +8,36 @@ resource "azurerm_virtual_network" "web" {
   name                = "module-network"
   count = var.instance_count
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.web.location
-  resource_group_name = azurerm_resource_group.web.name
+  location            = azurerm_resource_group.web[0].location
+  resource_group_name = azurerm_resource_group.web[0].name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   count = var.instance_count
-  resource_group_name  = azurerm_resource_group.web.name
-  virtual_network_name = azurerm_virtual_network.web.name
+  resource_group_name  = azurerm_resource_group.web[0].name
+  virtual_network_name = azurerm_virtual_network.web[0].name
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_network_interface" "snow" {
+resource "azurerm_network_interface" "web" {
   name                = "module-nic"
   count = var.instance_count
-  location            = azurerm_resource_group.web.location
-  resource_group_name = azurerm_resource_group.web.name
+  location            = azurerm_resource_group.web[0].location
+  resource_group_name = azurerm_resource_group.web[0].name
 
   ip_configuration {
     name                          = "snowconfig"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.internal[0].id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "module-vm"
-  location              = azurerm_resource_group.web.location
-  resource_group_name   = azurerm_resource_group.web.name
-  network_interface_ids = [azurerm_network_interface.snow.id]
+  location              = azurerm_resource_group.web[0].location
+  resource_group_name   = azurerm_resource_group.web[0].name
+  network_interface_ids = [azurerm_network_interface.web[0].id]
   vm_size               = var.instance_type
 
   count = var.instance_count
